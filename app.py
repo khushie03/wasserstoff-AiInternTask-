9872f8972911a7b586a9ca09ab7c7e8ecbdcefc8
Khushi Pandey <138788874+khushie03@users.noreply.github.com>
2024-10-04 13:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from main import *
 import os
-import json
-import fitz  
+import fitz
 import pandas as pd
-from main import tag_text, summarize_dialogue  
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -26,23 +24,23 @@ uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 if uploaded_file is not None:
     filename = uploaded_file.name
     filepath = os.path.join(UPLOAD_FOLDER, filename)
-    
+
     with open(filepath, "wb") as f:
         f.write(uploaded_file.getbuffer())
-    
+
     pdf_text = extract_text_from_pdf(filepath)
-    ner_tags_df = tag_text(pdf_text, tags, model, xlmr_tokenizer)  
-    summarized_text = summarize_dialogue(pdf_text) 
-    
-    ner_tags_df = pd.DataFrame(ner_tags_df)
-    ner_tags_df.columns = ['Token', 'Tag']
 
-    st.success('File successfully uploaded and processed!')
-    st.subheader("Summary")
-    st.write(summarized_text)
-    
-    st.subheader("NER Tags")
-    st.dataframe(ner_tags_df)
+    if pdf_text.strip():
+        ner_tags_df = tag_text(pdf_text, tags, model, xlmr_tokenizer)
+        summarized_text = summarize_dialogue(pdf_text)
+        ner_tags_df.columns = ['Token', 'Tag']
 
+        st.success('File successfully uploaded and processed!')
+        st.subheader("Summary")
+        st.write(summarized_text)
+        st.subheader("NER Tags")
+        st.dataframe(ner_tags_df)
+    else:
+        st.warning("The uploaded PDF file is empty or couldn't be processed.")
 else:
     st.warning("Please upload a PDF file.")
