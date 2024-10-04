b167604420e17719df0df34fa66931e83acfd5d3
Khushi Pandey <138788874+khushie03@users.noreply.github.com>
2024-10-04 13:28:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import pandas as pd
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 
-text_de = "Google Office is in London Google established in 10092 , Google working on artificiAL iNTELLIGENCE"
 tags = ['O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC']
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = AutoModelForTokenClassification.from_pretrained("Khushiee/xlm-roberta-base-finetuned-panx-ner-1").to(device)
 xlmr_tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-
 def tag_text(text, tags, model, tokenizer):
-    tokens = tokenizer(text).tokens()  
-    input_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)  
+    tokens = tokenizer.tokenize(text)  
+    input_ids = tokenizer(text, return_tensors="pt", max_length=512, truncation=True).input_ids.to(device)  
     outputs = model(input_ids)[0]  
     predictions = torch.argmax(outputs, dim=2)  
     preds = [tags[p] for p in predictions[0].cpu().numpy()]  
@@ -29,13 +27,14 @@ def tag_text(text, tags, model, tokenizer):
             seen_tokens.add(token)  
     return pd.DataFrame({'Tokens': filtered_tokens, 'Tags': filtered_preds})
 
+
 def summarize_dialogue(custom_dialogue):
     try:
         model_name = "Khushiee/pegasus-samsum-summarization"
         tokenizer = PegasusTokenizer.from_pretrained(model_name)
         model = PegasusForConditionalGeneration.from_pretrained(model_name)
 
-        inputs = tokenizer(custom_dialogue, return_tensors="pt", truncation=True)
+        inputs = tokenizer(custom_dialogue, return_tensors="pt", max_length=512, truncation=True)
         summary_ids = model.generate(inputs["input_ids"])
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
